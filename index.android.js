bc1eb59e212294951dbd05f04551a31ff2e7a4f6
axassin <jiobuaron17@gmail.com>
2017-06-19 07:02:44 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Text, AppRegistry} from 'react-native'
+
+const App = () => (
+    <Text>Wala man? yuga?</Text>
+  );
+
+
+AppRegistry.registerComponent('albums', () => App)
